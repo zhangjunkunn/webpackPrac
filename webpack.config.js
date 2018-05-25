272a6ae31e067762e98d842d43ad83f14052fbb2
zhangjunkunn <junkun.zhang@hotmail.com>
2018-05-25 20:37:48 +0800

    基础配置

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    }
+}
